06b31bfd4e89ebbc8aa9fc3ae647b52ef6554627
codesweaver <codetrain999@gmail.com>
2022-05-27 12:07:24 +0900

    mashimo

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-const baseURL = "https://www.rakuten.ne.jp/gold/toscana/side_menu_double.html";
-const baseHtml = "div#menumain > dl > dd";
+const baseURL = "https://www.rakuten.ne.jp/gold/mashimo/";
+const baseHtml = "li.side_nav-item";
 
 const getHtml = async () => {
     try {
@@ -18,15 +18,13 @@ getHtml().then((html) => {
     const $ = cheerio.load(html.data);
     const $categoryList = $(baseHtml);
 
-    // let categories = [];
     $categoryList.each(function(index, elem){
-
         const title = $(this).find("a").text().replace(/(\([0-9,]*\))+/g, '').replace(/(（[0-9,]*）)+/g, '').trim();
         const link = $(this).find("a").attr("href");
 
         const content = title + "\t" + link + "\n";
 
-        fs.appendFile("result.txt", content, error => {
+        fs.appendFile("mashimo.txt", content, error => {
             if (error) {
                 console.error(error);
                 return;
